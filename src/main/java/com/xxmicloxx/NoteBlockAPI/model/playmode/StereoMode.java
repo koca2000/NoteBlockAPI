82e1c1b246f179338aa5483bdedc5045b961381a
koca2000 <kocandrlev@gmail.com>
2020-03-30 00:28:52 +0200

    Ability to change StereoMode max distance from player

--- src/main/java/com/xxmicloxx/NoteBlockAPI/model/playmode/StereoMode.java
+++ src/main/java/com/xxmicloxx/NoteBlockAPI/model/playmode/StereoMode.java
@@ -30,4 +30,12 @@ public class StereoMode extends ChannelMode {
             CompatibilityUtils.playSound(player, location, InstrumentUtils.getInstrument(note.getInstrument()), soundCategory, volume, pitch, distance);
         }
     }
+
+    public float getMaxDistance() {
+        return maxDistance;
+    }
+
+    public void setMaxDistance(float maxDistance) {
+        this.maxDistance = maxDistance;
+    }
 }
